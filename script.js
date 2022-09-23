f13d86d9552d4a7d37c51bcc394fbe7512e6aac6
Jajam CH L Ramoji <87416110+jajamchlramoji@users.noreply.github.com>
2022-09-23 21:56:48 +0530

    Update with comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let weather = {
     "apiKey": "9512e76ff583b078e2c59c447d85a2cf",
     fetchWeather: function (city) {
+// Fetches Weather details from cities from api-"OpenWeatherMap"
         fetch("https://api.openweathermap.org/data/2.5/weather?q="
             + city
             + "&units=metric&appid="
@@ -8,6 +9,7 @@ let weather = {
         ).then((response) => response.json())
             .then((data) => this.displayWeather(data));
     },
+//     Fetches all the data from api's and feed it to the main file content individually
     displayWeather: function (data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
@@ -20,18 +22,19 @@ let weather = {
             "https://openweathermap.org/img/wn/"
             + icon
             + ".png";
-        // all the things in querySelector("...") names was the same names used in "class" in index.HTML
+        // all the things in querySelector("...") names was the same names used in "class" in index.HTML so all the details map exactly
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity : " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed : " + speed + "Km/hr";
         document.querySelector(".weather").classList.remove("loading")
     },
+
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
     },
 };
-
+// Searches for the Cities on clicking search button
 document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
 });
@@ -42,4 +45,4 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     }
 });
 
-weather.fetchWeather("Narasaraopet");
+// weather.fetchWeather("Narasaraopet");
